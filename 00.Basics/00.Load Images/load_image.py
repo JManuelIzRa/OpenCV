5e8a1634a4748fafeec8e485b2ec32194b277d97
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-05-19 00:33:52 +0200

    Load image and convert with line arguments options

--- 00.Basics/00.Load Images/load_image.py
+++ 00.Basics/00.Load Images/load_image.py
@@ -11,12 +11,14 @@ import cv2
 
 #Creamos la estructura que parsea los argumentos
 ap = argparse.ArgumentParser()
-ap.add_argument("-i","--image",required=True,help="path para añadir una imagen")
-args = vars(ap.parse_args())
+ap.add_argument("-i","--image", required=True, help="path para añadir una imagen")
+ap.add_argument("-o","--output", help="Muestra la imagen cargada", action="store_true")
+ap.add_argument("-c","--convert", required=True, help="Convierte la imagen a otro formato")
+args = ap.parse_args()
 
 #Cargamos la imagen desde el disk
-#La imagen a cargar se recoge usando args["image"]
-image = cv2.imread(args["image"])
+#La imagen a cargar se recoge usando args["image"] si usamos vars(ap.parse_args()), si no usamos args.image
+image = cv2.imread(args.image)
 
 #Obtenemos las dimensiones de la imagen, altura, anchura y número de canales
 # w x h; filas x columnas
@@ -27,6 +29,17 @@ print("Width: {} pixels".format(w))
 print("Height: {} pixels".format(h))
 print("Channels: {} pixels".format(c))
 
-#Muestra la imagen y espera la pulsación de una tecla
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+#Muestra la imagen y espera la pulsación de una tecla, siempre y cuando hayamos usado el argumento -o --output
+if args.output:
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+
+#Guardamos la imagen en disco (OpenCV convierte los tipos de imagen automáticamente)
+nueva_imagen = args.convert
+
+if args.convert:
+    try:
+        cv2.imwrite("newimage.{}".format(nueva_imagen), image)
+
+    except:
+        print("El formato introducido no es valido")
